9f226dc83ba9eb1d4e0747cf9af7a6e987c2e404
NDboy <endre.karsai@gmail.com>
2020-10-30 14:46:36 +0100

    first commit

---
+++ src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,21 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        System.out.println("Adj egy számot!");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+
+        System.out.println("Adj egy másik számot!");
+        int num2 = scanner.nextInt();
+
+        int num3 = num1 + num2;
+
+        System.out.println(num1 + "+" + num2);
+        System.out.println(num3);
+    }
+}
+//
