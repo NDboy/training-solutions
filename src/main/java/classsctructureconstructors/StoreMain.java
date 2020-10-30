ad7f0d6e86f86fd060c734a5136f2e548b9ee193
NDboy <endre.karsai@gmail.com>
2020-10-30 22:03:51 +0100

    8th commit

--- src/main/java/classsctructureconstructors/StoreMain.java
+++ src/main/java/classsctructureconstructors/StoreMain.java
@@ -12,10 +12,10 @@ public class StoreMain {
         store2.store(20);
         System.out.println(store2.getProduct() +" kezdő készlet: "+ store2.getStock());
 
-        store1.store(10);
-        store1.dispatch(15);
-        store2.store(17);
-        store2.dispatch(1);
+        store1.store(8);
+        store1.dispatch(5);
+        store2.store(7);
+        store2.dispatch(11);
 
         System.out.println("TV készlet jelenleg: " + store1.getStock());
         System.out.println("Rádió készlet jelenleg: " + store2.getStock());
