aa9e68ba5f84d0fe9101f19d03030158cb15f50f
NDboy <endre.karsai@gmail.com>
2020-11-24 16:13:53 +0100

    046

--- src/main/java/enumtype/week/Day.java
+++ src/main/java/enumtype/week/Day.java
@@ -1,22 +1,15 @@
 package enumtype.week;
 
-
 public enum Day {
-    MONDAY(DayType.WORKDAY),
-    TUESDAY(DayType.WORKDAY),
-    WEDNESDAY(DayType.WORKDAY),
-    THURSDAY(DayType.WORKDAY),
-    FRIDAY(DayType.WORKDAY),
-    SATURDAY(DayType.HOLIDAY),
-    SUNDAY(DayType.HOLIDAY);
+    MONDAY(DayType.WORKDAY), TUESDAY(DayType.WORKDAY), WEDNESDAY(DayType.WORKDAY), THURSDAY(DayType.WORKDAY), FRIDAY(DayType.WORKDAY), SATURDAY(DayType.HOLIDAY), SUNDAY(DayType.HOLIDAY);
 
-    private final DayType dayType;
+    private final DayType value;
 
-    Day(DayType dayType) {
-        this.dayType = dayType;
+    Day(DayType value) {
+        this.value = value;
     }
 
-    public DayType getDayType() {
-        return dayType;
+    public DayType getValue() {
+        return value;
     }
 }
