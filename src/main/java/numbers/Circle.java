3e95f7b4757ea710e8f74bd37675e0fe0a165fed
NDboy <endre.karsai@gmail.com>
2020-11-24 19:58:37 +0100

    048

--- src/main/java/numbers/Circle.java
+++ src/main/java/numbers/Circle.java
@@ -1,4 +1,4 @@
-package typeconversion;
+package numbers;
 
 public class Circle {
     private  int diameter;
--- src/main/java/numbers/CircleMain.java
+++ src/main/java/numbers/CircleMain.java
@@ -1,4 +1,6 @@
-package typeconversion;
+package numbers;
+
+import numbers.Circle;
 
 import java.util.ArrayList;
 import java.util.List;
