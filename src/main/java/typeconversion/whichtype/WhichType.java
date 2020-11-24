d66b5a5858d7dba1c762dd43a7aae57418ece065
NDboy <endre.karsai@gmail.com>
2020-11-24 18:15:41 +0100

    048

--- src/main/java/typeconversion/whichtype/WhichType.java
+++ src/main/java/typeconversion/whichtype/WhichType.java
@@ -1,7 +1,18 @@
 package typeconversion.whichtype;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WhichType {
 
-
-
+    public List<Type> whichType (String str) {
+        long longNumber = Long.parseLong(str);
+        List<Type> types = new ArrayList<>();
+        for (Type type : Type.values()) {
+            if ((type.getMin() <= longNumber) && (longNumber <= type.getMax())) {
+                types.add(type);
+            }
+        }
+        return types;
+    }
 }
---
+++ src/main/java/typeconversion/whichtype/WhichTypeMain.java
@@ -0,0 +1,8 @@
+package typeconversion.whichtype;
+
+public class WhichTypeMain {
+    public static void main(String[] args) {
+        System.out.println(new WhichType().whichType("23523568"));
+        System.out.println(new WhichType().whichType("23"));
+    }
+}
