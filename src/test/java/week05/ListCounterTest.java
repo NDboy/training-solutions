9d2c88c3fc84ea0ad155d06a1eae0325805b2e0e
NDboy <endre.karsai@gmail.com>
2020-11-25 16:26:30 +0100

    11.25

---
+++ src/test/java/week05/ListCounterTest.java
@@ -0,0 +1,38 @@
+package week05;
+
+import org.junit.jupiter.api.Test;
+import week05.week05d03.ListCounter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ListCounterTest {
+
+    @Test
+    public void wordCounterByATest() {
+        ListCounter listCounter = new ListCounter();
+        List<String> testList = Arrays.asList("alma", "körte", "ananász");
+
+        assertEquals(2, listCounter.wordCounterByA(testList));
+    }
+
+    @Test
+    public void wordCounterByAEmptyListTest() {
+        ListCounter listCounter = new ListCounter();
+        List<String> testList = new ArrayList<>();
+
+        assertEquals(0, listCounter.wordCounterByA(testList));
+    }
+
+    @Test
+    public void wordCounterByANonAListTest() {
+        ListCounter listCounter = new ListCounter();
+        List<String> testList = Arrays.asList("balma", "körte", "bananász");
+
+        assertEquals(0, listCounter.wordCounterByA(testList));
+    }
+
+}
