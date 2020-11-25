90d40d385ed0ab3ae4d3f2e59cfa57f64397b937
NDboy <endre.karsai@gmail.com>
2020-11-25 17:05:25 +0100

    11.25

--- src/main/java/introexception/patient/SsnValidator.java
+++ src/main/java/introexception/patient/SsnValidator.java
@@ -3,6 +3,9 @@ package introexception.patient;
 public class SsnValidator {
 
     public boolean isValidSsn (String ssn) {
+        if (ssn.length() != 9)
+            return false;
+
         boolean isValidLastNumberOfSsn = Integer.parseInt(ssn.substring(8))==(Integer.parseInt(ssn.substring(0,1)) * 3 + Integer.parseInt(ssn.substring(1,2)) * 7 + Integer.parseInt(ssn.substring(2,3)) * 3 + Integer.parseInt(ssn.substring(3,4)) * 7 + Integer.parseInt(ssn.substring(4,5)) * 3 + Integer.parseInt(ssn.substring(5,6)) * 7 + Integer.parseInt(ssn.substring(6,7)) * 3 + Integer.parseInt(ssn.substring(7,8)) * 7)%10;
         try {
             int value = Integer.parseInt(ssn);
