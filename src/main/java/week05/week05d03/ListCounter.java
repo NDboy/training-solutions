6e96aa0c20837d09589579f4b27a2876fad3de68
NDboy <endre.karsai@gmail.com>
2020-11-25 16:32:04 +0100

    11.25

--- src/main/java/week05/week05d03/ListCounter.java
+++ src/main/java/week05/week05d03/ListCounter.java
@@ -1,13 +1,13 @@
 package week05.week05d03;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ListCounter {
 
     public int wordCounterByA(List<String> words) {
-        if (words.isEmpty()) {
-            return 0;
-        }
+
         int counter = 0;
         for (String word : words) {
             if (word.toLowerCase().indexOf("a") == 0) {
@@ -19,7 +19,14 @@ public class ListCounter {
 
     }
 
+    public static void main(String[] args) {
+        ListCounter listCounter = new ListCounter();
+        List<String> words = Arrays.asList("alma", "körte", "ananász");
+//        List<String> words = new ArrayList<>();
+
+        System.out.println(listCounter.wordCounterByA(words));
+
+    }
+
 }
-//    Írj egy olyan metódust az week05d03.ListCounter osztályban, mely egy kapott Listából (String) összeszámolja azokat amik 'a' vagy 'A' betűvel kezdődnek,
-//    Ha nulla elemű a tömb,vagy nincs benne ilyen elem, akkor 0-t adjon vissza. (edited)
 
