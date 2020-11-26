fb9a8438bc1ecf3eb131cc2b001b113e24d6a397
NDboy <endre.karsai@gmail.com>
2020-11-26 18:23:42 +0100

    11.26

--- src/main/java/stringscanner/StringScanner.java
+++ src/main/java/stringscanner/StringScanner.java
@@ -1,45 +1,62 @@
 package stringscanner;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class StringScanner {
-    private String intString;
-    private String delimiter;
-    private String text;
-    private String word;
 
     private boolean isEmpty(String s) {
-        return s == null || "".equals(s.trim());
+        return s == null || s.isBlank();
     }
 
-    public StringScanner(String intString, String delimiter, String text, String word) {
-        if (intString.isEmpty()) {
-            throw new IllegalArgumentException("Invalid argument.");
-        }
-        this.intString = intString;
-        if (delimiter.isEmpty()) {
-            throw new IllegalArgumentException("Invalid argument.");
-        }
-        this.delimiter = delimiter;
-        if (text.isEmpty()) {
-            throw new IllegalArgumentException("Invalid argument.");
-        }
-        this.text = text;
-        if (word.isEmpty()) {
-            throw new IllegalArgumentException("Invalid argument.");
-        }
-        this.word = word;
-    }
 
     public int readAndSumValues(String intString, String delimiter) {
-        Scanner s = new Scanner(intString).useDelimiter(delimiter);
+        if (isEmpty(intString) || delimiter == null) {
+            throw new IllegalArgumentException("Incorrect parameter string!");
+        }
+        Scanner sc = new Scanner(intString).useDelimiter(delimiter);
         int sum = 0;
-        while (s.hasNextInt()) {
-            sum = sum + s.nextInt();
+        try {
+            while (sc.hasNext()) {
+                sum += sc.nextInt();
+            }
+        } catch (InputMismatchException ex) {
+            throw new IllegalArgumentException("Incorrect parameter string!", ex);
         }
         return sum;
-
     }
 
 
+    public int readAndSumValues(String intString) {
+        if (isEmpty(intString)) {
+            throw new IllegalArgumentException("Incorrect parameter string!");
+        }
+        Scanner sc = new Scanner(intString);
+        int sum = 0;
+        try {
+            while (sc.hasNext()) {
+                sum += sc.nextInt();
+            }
+        } catch (InputMismatchException ex) {
+            throw new IllegalArgumentException("Incorrect parameter string!", ex);
+        }
+        return sum;
+    }
+
+
+    public String filterLinesWithWordOccurrences(String text, String word) {
+        if (isEmpty(text) || word == null || "".equals(word)) {
+            throw new IllegalArgumentException("Incorrect parameter string!");
+        }
+        StringBuilder newTextWithOccurrences = new StringBuilder();
+        Scanner sc = new Scanner(text);
+        String line = "";
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            if (line.contains(word)) {
+                newTextWithOccurrences.append(line + "\n");
+            }
+        }
+        return newTextWithOccurrences.toString().trim();
+    }
 }
