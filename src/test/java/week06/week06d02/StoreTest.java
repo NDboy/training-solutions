911a43ae199ddf31c41433cd9f200f06dd82fe68
NDboy <endre.karsai@gmail.com>
2020-12-01 16:04:44 +0100

    órai feladat

---
+++ src/test/java/week06/week06d02/StoreTest.java
@@ -0,0 +1,29 @@
+package week06.week06d02;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StoreTest {
+
+
+    @Test
+    public void testGetProductByCategoryName1() {
+        assertEquals(1, new Store(Arrays.asList(new Product("alma", Category.DAIRY, 125), new Product("májkrém", Category.OTHER, 305))).getProductByCategoryName(Category.DAIRY));
+    }
+
+    @Test
+    public void testGetProductByCategoryName2() {
+        assertEquals(1, new Store(Arrays.asList(new Product("alma", Category.DAIRY, 125), new Product("májkrém", Category.OTHER, 305))).getProductByCategoryName(Category.OTHER));
+    }
+
+    @Test
+    public void testGetProductByCategoryName3() {
+        assertEquals(0, new Store(Arrays.asList(new Product("alma", Category.DAIRY, 125), new Product("májkrém", Category.OTHER, 305))).getProductByCategoryName(Category.FROZEN));
+    }
+
+
+}
