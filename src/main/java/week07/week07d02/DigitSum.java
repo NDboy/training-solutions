dae72b5bf2b0cd5bd04bfd749febc62fced2ed16
NDboy <endre.karsai@gmail.com>
2020-12-08 15:46:01 +0100

    12.08 Órai feladat teszt nélkül

---
+++ src/main/java/week07/week07d02/DigitSum.java
@@ -0,0 +1,33 @@
+package week07.week07d02;
+
+public class DigitSum {
+    public int sumOfDigitsString(int x){
+        if (x < 0) {
+            throw new IllegalArgumentException("Wrong argument!");
+        }
+        String str = Integer.toString(x);
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            sum += Integer.parseInt(str.substring(i, i + 1));
+        }
+        return sum;
+    }
+
+    public int sumOfDigitsByRoundingLoss(int x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("Wrong argument!");
+        }
+        int divMult = x;
+        int sum = 0;
+        if (x < 10) {
+            return x;
+        } else {
+            while (divMult > 0) {
+                divMult = x / 10 * 10;
+                sum += x - divMult;
+                x /= 10;
+            }
+            return sum;
+        }
+    }
+}
