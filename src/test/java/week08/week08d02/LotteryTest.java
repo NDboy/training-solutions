479b045cd1f9a2777ea150a6e7b583af3f61b855
NDboy <endre.karsai@gmail.com>
2020-12-15 15:46:20 +0100

    12.15. n parameter a teszteléshez seednek

---
+++ src/test/java/week08/week08d02/LotteryTest.java
@@ -0,0 +1,22 @@
+package week08.week08d02;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LotteryTest {
+
+    @Test
+    public void testGetNumbers() {
+        assertEquals(5, Lottery.getNumbers(90, 5, 6).size());
+        assertEquals(6, Lottery.getNumbers(90, 6, 6).size());
+        assertEquals(Arrays.asList(62,7,27,19,2,54), Lottery.getNumbers(90, 6, 6));
+
+    }
+
+
+
+
+}
