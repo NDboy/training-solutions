e325c568b4d2b8242abb4d0ac2cfe8c00cda92fb
NDboy <endre.karsai@gmail.com>
2020-12-16 15:40:42 +0100

    12.16 teszt nélkül

---
+++ src/main/java/week08/week08d03/StringLists.java
@@ -0,0 +1,22 @@
+package week08.week08d03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringLists {
+
+    public List<String> stringListsUnion(List<String> first, List<String> second) {
+        List<String> result = new ArrayList<>();
+        for (String f: first) {
+            if (!result.contains(f)) {
+                result.add(f);
+            }
+        }
+        for (String s: second) {
+            if (!result.contains(s)) {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+}
