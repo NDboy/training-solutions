50c79565805d38af90a62b436a445e3ae65b6671
NDboy <endre.karsai@gmail.com>
2021-01-05 16:09:01 +0100

    01.05. órai feladat

---
+++ src/main/java/week10/week10d02/MaxTravel.java
@@ -0,0 +1,24 @@
+package week10.week10d02;
+
+import java.util.List;
+
+public class MaxTravel {
+    public int getMaxIndex(List<Integer> passengerList) {
+        int[] passengerArray = new int[30];
+        int result = 0;
+        for (int i = 0; i < passengerList.size() ; i++) {
+            passengerArray[passengerList.get(i)] ++;
+        }
+        for (int item: passengerArray) {
+            if (item > result) {
+                result = item;
+            }
+        }
+        return result;
+    }
+}
+
+//    Van egy egész számok listája, amelyben egy szám azt jelzi, hogy az adott számú buszmegállóban
+//    egy ember fel akar szállni. A 12,12,0,3,4,4 sorozat tehát azt jelenti,
+//    hogy a 12-es buszmegállóban 2-en, a 0-ásban 1 ember, 3-asban egy ember, 4-esben 2 ember akar felszállni.
+//    A MaxTravel osztály getMaxIndex() metódusa adja vissza, hogy hanyas megállóban szeretnének a legtöbben felszállni. Maximum 30 megálló lehet.
