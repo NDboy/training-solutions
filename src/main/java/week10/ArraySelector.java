456059f36a6a576bf1650a5f6dac99dcfcb1100c
NDboy <endre.karsai@gmail.com>
2021-01-06 20:26:34 +0100

    vizsga feladat 1.

--- src/main/java/week10/ArraySelector.java
+++ src/main/java/week10/ArraySelector.java
@@ -1,6 +1,23 @@
 package week10;
 
 public class ArraySelector {
+
+    public String selectEvens(int [] numbers) {
+        if (numbers.length == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == 0 || i % 2 == 0) {
+                sb.append(numbers[i] + ", ");
+            }
+        }
+        sb.delete(sb.length()-2, sb.length());
+        sb.append("]");
+        return sb.toString();
+    }
+
 }
 
 //    Írj egy olyan metódust az ArraySelector osztályban, mely egy kapott tömbből minden második elemet kiválogat, és egy szövegként összerak, szögletes zárójelek között!
