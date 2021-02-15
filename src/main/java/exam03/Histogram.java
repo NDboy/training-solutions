0ac3803621913367e86811a6b9894146f7e90abc
NDboy <endre.karsai@gmail.com>
2021-02-15 19:33:48 +0100

    02.15 Histogram megoldás

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,4 +1,25 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Histogram {
+
+    public String createHistogram(BufferedReader reader) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null){
+                int number = Integer.parseInt(line);
+                for (int i = 0; i < number; i++) {
+                    stringBuilder.append('*');
+                }
+                stringBuilder.append("\n");
+            }
+        } catch (
+                IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+        return stringBuilder.toString();
+    }
 }
